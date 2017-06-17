90a510cd2dc984ff69c82ac836cf0782ee91b3c6
Narusuk <Robin.Ruffiner@gmail.com>
2017-06-17 23:52:33 +0200

    BanqueDialogChangement
    accountgest.sqf a revoir

--- Kira_dialog/dialog/Banque.hpp
+++ Kira_dialog/dialog/Banque.hpp
@@ -1,4 +1,4 @@
-class Life_AccountReq {
+class Life_AccountRequest {
 	idd = 11000;
 	name= "life_AccountRequest";
 	movingEnable = false;
@@ -6,99 +6,81 @@ class Life_AccountReq {
 
 	class controlsBackground
 	{
-		class Background: Life_RscText
-		{
-			idc = 1000;
-			text = "";
-			x = 0.1875;
-			y = 0.3;
-			w = 0.6375;
-			h = 0.46;
-			colorBackground[] = {0,0,0,0.7};
-		};
-		class TitleBox: Life_RscText
-		{
-			idc = 1001;
-			text = "Creation d'un compte en banque";
-			x = 0.1875;
-			y = 0.26;
-			w = 0.6375;
-			h = 0.04;
-			colorBackground[] = {0.38,0.7,0.88,0.7};
-		};
-	};
-	class controls
-	{
-		class close: Life_RscButtonMenu
-		{
-			idc = 11001;
-			text = "FERMER"; 
-            onButtonClick = "closeDialog 0;";
-			x = 0.1875;
-			y = 0.76;
-			w = 0.15;
-			h = 0.04;
-			colorBackground[] = {0,0,0,1};
-		};
-		class Create: Life_RscButtonMenu
-		{
-			idc = 11002;
-			text = "Soumettre";
-            onButtonClick = "2 spawn life_fnc_requestAccount;";
-			x = 0.675;
-			y = 0.76;
-			w = 0.15;
-			h = 0.04;
-			colorBackground[] = {0,0,0,1};
-		};
-		class Reglement: Life_RscStructuredText
-		{
-			idc = 11003;
-			text = "Vous ne pouvez avoir qu'un seul compte";
-			x = 0.2;
-			y = 0.32;
-			w = 0.6125;
-			h = 0.24;
-			colorBackground[] = {0,0,0,0.9};
-		};
-		class TextCheckB: Life_RscText
-		{
-			idc = 11004;
-			text = "Entreprise ?"; 
-			x = 0.204489;
-			y = 0.58819;
-			w = 0.175;
-			h = 0.06;
-            sizeEx = 0.04;
-		};
-		class EntrepriseCheckbox: Life_Checkbox
-		{
-			idc = 11005;
-            type = 77;
-			x = 0.375;
-			y = 0.6;
-			w = 0.0375;
-			h = 0.04;
-		};
-		class TextEdit: Life_RscText
-		{
-			idc = 11006;
-			text = "Nom du compte :"; 
-			x = 0.206409;
-			y = 0.66;
-			w = 0.2125;
-			h = 0.06;
-            sizeEx = 0.04;
-		};
-		class EditName: Life_RscEdit
-		{
-			idc = 11007;
-			text = ""; 
-			x = 0.4375;
-			y = 0.67181;
-			w = 0.365705;
-			h = 0.04819;
-			colorBackground[] = {0,0,0,1};
-		};
-	};
-};
+      class BackGround: Life_RscText
+      {
+      	idc = -1;
+      	x = 0.075;
+      	y = 0.08;
+      	w = 0.85;
+      	h = 0.82;
+      	colorBackground[] = {-1,-1,-1,0.5};
+      };
+      class BackGroundTitle: Life_RscText
+      {
+      	idc = -1;
+      	text = "Création de compte en banque";
+      	x = 0.075;
+      	y = 0.08;
+      	w = 0.85;
+      	h = 0.04;
+      	colorBackground[] = {0,0.49,0.49,1};
+      };
+  };
+
+ class controls {
+      class NameAccount: Life_RscText
+      {
+      	idc = 11006;
+      	text = "Nom du compte :";
+      	x = 0.103662;
+      	y = 0.190101;
+      	w = 0.55;
+      	h = 0.06;
+      };
+      class AccoutNAMEENTER: Life_RscEdit
+      {
+      	idc = 11007;
+      	x = 0.1125;
+      	y = 0.26;
+      	w = 0.7625;
+      	h = 0.04;
+      	colorBackground[] = {-1,-1,-1,1};
+      	colorActive[] = {-1,-1,-1,1};
+      };
+      class EnterCheck: Life_Checkbox
+      {
+      	idc = 11005;
+      	x = 0.1125;
+      	y = 0.64;
+      	w = 0.0375;
+      	h = 0.04;
+      };
+      class Rules: Life_RscStructuredText
+      {
+      	idc = 11001;
+      	text = "Si , vous devez créer un compte entreprise vous devez avoir été autoriser par les administrations , si ce n'est pas le cas tout votre argent présent sur ce compte lors de la mise en arrêt , sera gard et transféré dans un compte gouvernemental , de plus un procès verbal sera engagé , avec l'obligation de payer une amende avec le montant qu'il y avait sur le compte .";
+      	y = 0.34;
+      	w = 0.7625;
+      	h = 0.22;
+      	colorBackground[] = {-1,-1,-1,1};
+      };
+      class TextCheckB: Life_RscText
+      {
+      	idc = 11004;
+      	text = "Entreprise";
+      	x = 0.15;
+      	y = 0.64;
+      	w = 0.1;
+      	h = 0.04;
+      };
+      class ButtonSum: Life_RscButtonMenu
+      {
+      	idc = 11002;
+      	text = "Soumettre le compte";
+          onButtonClick = "2 spawn life_fnc_requestAccount;";
+      	x = 0.525;
+      	y = 0.8;
+      	w = 0.3625;
+      	h = 0.06;
+      };
+  };
