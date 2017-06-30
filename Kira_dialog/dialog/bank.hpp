cb9f2125c0169cc8064299b73937773a729b521f
kira370 <thekirahd@gmail.com>
2017-06-30 17:39:36 +0200

    changement bank dialog, plus organisé

--- Kira_dialog/dialog/bank.hpp
+++ Kira_dialog/dialog/bank.hpp
@@ -13,7 +13,7 @@ class Life_atm_management {
 				x = 0.375;
 				y = 0.08;
 				w = 0.3375;
-				h = 0.88;
+				h = 0.77;
 				colorBackground[] = {-1,-1,-1,0.7};
 			};
 			class BarpresentATM: Life_RscText
@@ -31,7 +31,7 @@ class Life_atm_management {
 				idc = -1;
 				text = "TRANSFERT";
 				x = 0.375;
-				y = 0.56;
+				y = 0.56 - 0.07;
 				w = 0.3375;
 				h = 0.04;
 				colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])","(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])","(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])","(profilenamespace getvariable ['GUI_BCG_RGB_A',0.7])"};
@@ -53,16 +53,16 @@ class Life_atm_management {
 			idc = 2702;
 			text = "";
 			x = 0.4125;
-			y = 0.4;
+			y = 0.33;
 			w = 0.2625;
 			h = 0.04;
 		};
 		class Deposit: Life_RscButtonMenu
 		{
 			idc = -1;
-			text = "$STR_ATM_Deposit";
+			text = "Deposer";
 			x = 0.4125;
-			y = 0.48;
+			y = 0.48 - 0.07;
 			w = 0.125;
 			h = 0.04;
       onButtonClick = "[] call life_fnc_bankDeposit";
@@ -71,9 +71,9 @@ class Life_atm_management {
 		class Witdrawn: Life_RscButtonMenu
 		{
 			idc = -1;
-			text = "$STR_ATM_Withdraw";
+			text = "Retirer";
 			x = 0.5625;
-			y = 0.48;
+			y = 0.48 - 0.07;
 			w = 0.125;
 			h = 0.04;
       onButtonClick = "[] call life_fnc_bankWithdraw";
@@ -83,7 +83,7 @@ class Life_atm_management {
 		{
 			idc = 2780;
 			x = 0.4;
-			y = 0.66;
+			y = 0.66 - 0.07;
 			w = 0.2875;
 			h = 0.04;
 		};
@@ -92,19 +92,20 @@ class Life_atm_management {
 			idc = 2750;
 			text = "";
 			x = 0.4;
-			y = 0.74;
+			y = 0.74 - 0.07;
 			w = 0.2875;
 			h = 0.04;
 		};
 		class Transfert: Life_RscButtonMenu
 		{
 			idc = -1;
-			text = "$STR_ATM_Transfer";
-			x = 0.4875;
-			y = 0.82;
-			w = 0.125;
+			text = "Transferer";
+			x = 0.375 + ((0.3375/2) - (0.17/2));
+			y = 0.82 - 0.07;
+			w = 0.17;
 			h = 0.04;
-      onButtonClick = "[] call life_fnc_bankTransfer";
+			sizeEx = "0.03";
+      		onButtonClick = "[] call life_fnc_bankTransfer";
 			colorBackground[] = {"(profilenamespace getvariable ['GUI_BCG_RGB_R',0.3843])","(profilenamespace getvariable ['GUI_BCG_RGB_G',0.7019])","(profilenamespace getvariable ['GUI_BCG_RGB_B',0.8862])",0.5};
 		};
 	};
