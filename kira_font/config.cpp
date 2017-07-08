0034061fcb6f652e5447fe14b1086f7871da4a02
Thesam1798 <debris.alex@gmail.com>
2017-07-08 23:52:45 +0200

    Add Kira Font

---
+++ kira_font/config.cpp
@@ -0,0 +1,26 @@
+class CfgPatches
+{
+	class Kira_Font
+	{
+		requiredVersion = 0.1;
+		requiredAddons[] = {};
+		units[] = {};
+		weapons[] = {};
+		magazines[] = {};
+		ammo[] = {};
+	};
+};
+class CfgFontFamilies{
+	class OrbitronLight
+	{
+		spaceWidth = 0.7;
+		spacing = 0.065;
+		fonts[] = {"\Kira_Font\OrbitronLight\OrbitronLight6","\Kira_Font\OrbitronLight\OrbitronLight7","\Kira_Font\OrbitronLight\OrbitronLight8","\Kira_Font\OrbitronLight\OrbitronLight9","\Kira_Font\OrbitronLight\OrbitronLight10","\Kira_Font\OrbitronLight\OrbitronLight11","\Kira_Font\OrbitronLight\OrbitronLight12","\Kira_Font\OrbitronLight\OrbitronLight13","\Kira_Font\OrbitronLight\OrbitronLight14","\Kira_Font\OrbitronLight\OrbitronLight15","\Kira_Font\OrbitronLight\OrbitronLight16","\Kira_Font\OrbitronLight\OrbitronLight17","\Kira_Font\OrbitronLight\OrbitronLight18","\Kira_Font\OrbitronLight\OrbitronLight19","\Kira_Font\OrbitronLight\OrbitronLight20","\Kira_Font\OrbitronLight\OrbitronLight21","\Kira_Font\OrbitronLight\OrbitronLight22","\Kira_Font\OrbitronLight\OrbitronLight23","\Kira_Font\OrbitronLight\OrbitronLight24","\Kira_Font\OrbitronLight\OrbitronLight25","\Kira_Font\OrbitronLight\OrbitronLight26","\Kira_Font\OrbitronLight\OrbitronLight27","\Kira_Font\OrbitronLight\OrbitronLight28","\Kira_Font\OrbitronLight\OrbitronLight29","\Kira_Font\OrbitronLight\OrbitronLight30","\Kira_Font\OrbitronLight\OrbitronLight31","\Kira_Font\OrbitronLight\OrbitronLight34","\Kira_Font\OrbitronLight\OrbitronLight35","\Kira_Font\OrbitronLight\OrbitronLight37","\Kira_Font\OrbitronLight\OrbitronLight46"};
+	};
+	class OrbitronMedium
+	{
+		spaceWidth = 0.7;
+		spacing = 0.065;
+		fonts[] = {"\Kira_Font\OrbitronMedium\OrbitronMedium6","\Kira_Font\OrbitronMedium\OrbitronMedium7","\Kira_Font\OrbitronMedium\OrbitronMedium8","\Kira_Font\OrbitronMedium\OrbitronMedium9","\Kira_Font\OrbitronMedium\OrbitronMedium10","\Kira_Font\OrbitronMedium\OrbitronMedium11","\Kira_Font\OrbitronMedium\OrbitronMedium12","\Kira_Font\OrbitronMedium\OrbitronMedium13","\Kira_Font\OrbitronMedium\OrbitronMedium14","\Kira_Font\OrbitronMedium\OrbitronMedium15","\Kira_Font\OrbitronMedium\OrbitronMedium16","\Kira_Font\OrbitronMedium\OrbitronMedium17","\Kira_Font\OrbitronMedium\OrbitronMedium18","\Kira_Font\OrbitronMedium\OrbitronMedium19","\Kira_Font\OrbitronMedium\OrbitronMedium20","\Kira_Font\OrbitronMedium\OrbitronMedium21","\Kira_Font\OrbitronMedium\OrbitronMedium22","\Kira_Font\OrbitronMedium\OrbitronMedium23","\Kira_Font\OrbitronMedium\OrbitronMedium24","\Kira_Font\OrbitronMedium\OrbitronMedium25","\Kira_Font\OrbitronMedium\OrbitronMedium26","\Kira_Font\OrbitronMedium\OrbitronMedium27","\Kira_Font\OrbitronMedium\OrbitronMedium28","\Kira_Font\OrbitronMedium\OrbitronMedium29","\Kira_Font\OrbitronMedium\OrbitronMedium30","\Kira_Font\OrbitronMedium\OrbitronMedium31","\Kira_Font\OrbitronMedium\OrbitronMedium34","\Kira_Font\OrbitronMedium\OrbitronMedium35","\Kira_Font\OrbitronMedium\OrbitronMedium37","\Kira_Font\OrbitronMedium\OrbitronMedium46"};
+	};
+};
