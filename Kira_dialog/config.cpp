3e50be21dd8f882e897fc4f4411d090a12998647
kira370 <thekirahd@gmail.com>
2017-08-04 15:14:21 +0200

    a

--- Kira_dialog/config.cpp
+++ Kira_dialog/config.cpp
@@ -4147,7 +4147,6 @@ class DOSTARA_BOURSE
 #include "dialog\gang.hpp"
 #include "dialog\key_chain.hpp"
 #include "dialog\impound.hpp"
-#include "dialog\impoundcop.hpp"
 #include "dialog\bank.hpp"
 #include "dialog\veh_shop.hpp"
 #include "dialog\shops.hpp"
